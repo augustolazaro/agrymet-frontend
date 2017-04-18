da4cdf81df5ba9592903ff01a6325ef88e71a1f0
augustolazaro <aemlazaro@gmail.com>
2017-04-18 15:09:09 -0300

    Materialize configuration

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar, NavItem, Icon } from 'react-materialize';
+import '../stylesheets/css/materialize.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar brand='Agrymet' right>
+        <NavItem href='/'>CADASTRE-SE</NavItem>
+        <NavItem href='/'>LOGIN</NavItem>
+        <NavItem href='/'><Icon>more_vert</Icon></NavItem>
+      </Navbar>
+    );
+  }
+}
+
+export default App;
